cd670960ef3ce5c74040955f4c17bc271e9f5d4b
amirboulder <amsa3731@colorado.edu>
2022-11-21 13:37:53 -0700

    refactored renderer class

--- RayTracing/src/WalnutApp.cpp
+++ RayTracing/src/WalnutApp.cpp
@@ -2,8 +2,11 @@
 #include "Walnut/EntryPoint.h"
 
 #include "Walnut/Image.h"
-#include "Walnut/Random.h"
+
 #include "Walnut/Timer.h"
+
+#include "../Renderer.h"
+
 using namespace Walnut;
 
 class ExampleLayer : public Walnut::Layer
@@ -13,6 +16,7 @@ public:
 	{
 		ImGui::Begin("Settings");
 		ImGui::Text("Last render: %.3fms", m_LastRenderTime);
+		ImGui::Text("Resolution : %1fms", ImGui::GetContentRegionAvail().x * ImGui::GetContentRegionAvail().y);
 		if (ImGui::Button("Render")) {
 			Render();
 		};
@@ -25,8 +29,9 @@ public:
 		m_ViewportWidth = ImGui::GetContentRegionAvail().x;
 		m_ViewportHeight = ImGui::GetContentRegionAvail().y;
 
-		if(m_Image){
-			ImGui::Image(m_Image->GetDescriptorSet(), {(float)m_Image->GetWidth(), (float)m_Image->GetHeight() });
+		auto image = m_renderer.GetFinalImage();
+		if(image){
+			ImGui::Image(image->GetDescriptorSet(), {(float)image->GetWidth(), (float)image->GetHeight() });
 		}
 		ImGui::End();
 		ImGui::PopStyleVar();
@@ -37,22 +42,18 @@ public:
 
 	void Render() {
 		Timer timer;
-		if (!m_Image || m_ViewportWidth != m_Image->GetWidth() || m_ViewportHeight != m_Image->GetHeight()) {
-			m_Image = std::make_shared<Image>(m_ViewportWidth, m_ViewportHeight, ImageFormat::RGBA);
-			delete[] m_ImageData;
-			m_ImageData = new uint32_t[m_ViewportWidth * m_ViewportHeight];
-		}
-		for (uint32_t i = 0; i < m_ViewportWidth * m_ViewportHeight; i++) {
-			m_ImageData[i] = Random::UInt();
-			m_ImageData[i] |= 0xff000000;
-		}
-		m_Image->SetData(m_ImageData);
+
+		m_renderer.OnResize(m_ViewportWidth, m_ViewportHeight);
+		m_renderer.Render();
+	
+	
+		
 
 		m_LastRenderTime = timer.ElapsedMillis();
 	}
 private:
-	std::shared_ptr<Image> m_Image;
-	uint32_t* m_ImageData = nullptr;
+	Renderer m_renderer;
+	
 	uint32_t m_ViewportWidth = 0;
 	uint32_t m_ViewportHeight = 0;
 	float m_LastRenderTime = 0.0f;
